db9b4c9c05b26683190a51e92392462ca1db1f79
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2014-07-09 23:52:32 +0200

    played boy-scout and cleaned up a bit

---
+++ buildconfig.groovy
@@ -0,0 +1,20 @@
+sourcePath = './src/DE/asciidoc/'
+targetPath = './src_gen/DE/asciidoc/'
+
+goldenMaster {
+    // a list of all features contained in the golden master
+    allFeatures = ['help', 'example']
+
+    // style: list of features
+    templateStyles = [
+            'plain'    : [],
+            'with-help': ['help'],
+            // deaktivated for the moment - no content yet
+            // 'with-examples':['help','example'],
+    ]
+}
+
+distribution {
+    targetPath = "build/dist/"
+    formats = ['asciidoc','html','epub','markdown','docx','docbook']
+}
